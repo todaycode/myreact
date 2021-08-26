866d0d3cc8a6683a93eb7545c3eb4385e4f35816
todaycode <onetoday1030@gmail.com>
2021-08-27 00:47:09 +0900

    material UI removed

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ export const QueryContext = React.createContext();
 export default function App() {
   const [query, setQuery] = useState("git");
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setIsLoading(true);
     query != ""
       ? fetch(`https://api.github.com/search/users?q=${query}`)
           .then((res) => res.json())
-          .then((res) => setUsers(res.items))
+          .then((res) => {
+            setUsers(res.items);
+            setIsLoading(false);
+          })
           .catch((err) => setUsers([]))
       : setUsers([]);
   }, [query]);
@@ -20,10 +25,12 @@ export default function App() {
     <div className="App">
       <QueryContext.Provider value={{ query, setQuery }}>
         <Header />
-        <div className="wrapper">
-          {users.map((user) => (
-            <UserCard user={user} />
-          ))}
+        <div className="container users">
+          {!isLoading ? (
+            users.map((user) => <UserCard key={user.id} user={user} />)
+          ) : (
+            <h2>'Loading...'</h2>
+          )}
         </div>
       </QueryContext.Provider>
     </div>
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,44 +1,13 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-    margin: '20px'
-  },
-  media: {
-    height: 140
-  }
-});
+import './UserCard.css';
 
 export default function GitUserCard(props) {
-  const classes = useStyles();
   const { user } = props;
 
   return (
-    <Card className={classes.root} onClick={e => window.open(user.html_url,'_blank')}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={user.avatar_url}
-          title={user.name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {user.login}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {user.html_url}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+      <div className="card" title={user.login} onClick={e => window.open(user.html_url,'_blank')}>
+          <img className="avatar" src={user.avatar_url} height="140px"/>
+          <h3 className="username">{user.login}</h3>
+      </div>
   );
 }
