0ac20be85e0e5c1f36548bfbfc717f154893f59a
todaycode <onetoday1030@gmail.com>
2021-08-26 19:48:14 +0900

    axios removed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import UserCard from './components/UserCard';
+import UserCard from "./components/UserCard";
 
 export const QueryContext = React.createContext();
 
@@ -11,15 +10,15 @@ export default function App() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
     query != ""
-      ? axios
-          .get(`https://api.github.com/search/users?q=${query}`)
-          .then((res) => setUsers(res.data.items))
+      ? fetch(`https://api.github.com/search/users?q=${query}`)
+          .then((res) => res.json())
+          .then((res) => setUsers(res.items))
           .catch((err) => setUsers([]))
       : setUsers([]);
   }, [query]);
   return (
     <div className="App">
-      <QueryContext.Provider value={{query, setQuery}}>
+      <QueryContext.Provider value={{ query, setQuery }}>
         <Header />
         <div className="wrapper">
           {users.map((user) => (
