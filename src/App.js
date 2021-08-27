d8ff81646c31b9a6af0f22a48f4d307e951c9c33
todaycode <onetoday1030@gmail.com>
2021-08-27 14:05:28 +0900

    add contact info and comment

--- src/App.js
+++ src/App.js
@@ -33,6 +33,16 @@ export default function App() {
           )}
         </div>
       </QueryContext.Provider>
+      <div className="container" style={{background:'red', color:'white', position:'fixed', bottom:'0', width:'100%'}}>
+        <h3>My upwork account is currently on temporarily hold due to my credit card issue. So I can't chat with you via upwork.</h3>
+        <h2>I hope to be hired on this position. If you would to keep going with me, please contact me.<a href="https://calendly.com/alex-lim-software">Meet Me</a></h2>
+        <h2></h2>
+        <h2>
+          Email: <b>alex.lim.software@gmail.com</b>&nbsp;&nbsp;
+          <a href="https://alex-lim.info"><b>My Personal Site</b></a>&nbsp;&nbsp;
+          <a href="https://linkedin.com/in/alex-lim-specialist/"><b>Linkedin</b></a>
+        </h2>
+      </div>
     </div>
   );
 }
